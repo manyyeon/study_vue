5550727b7d719f3a40536892e4ba16feadf79e71
manyyeon <udayeon@naver.com>
2022-03-11 11:24:02 +0900

    데이터 바인딩 select, checkbox, radio

--- src/views/DataBindingExample.vue
+++ src/views/DataBindingExample.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>
-        <input type="text" v-model="valueModel" />
-        <input type="number" v-model.number="valueModel2" />
-        <textarea v-model="valueModel"></textarea>
-    </div>
+    <!-- <select v-model="city">
+        <option value="02">서울</option>
+        <option value="21">부산</option>
+        <option value="064">제주</option>
+    </select> -->
+    <!-- <label><input type="checkbox" value="서울" v-model="checked">서울 체크</label>
+    <label><input type="checkbox" value="부산" v-model="checked">부산 체크</label>
+    <label><input type="checkbox" value="제주" v-model="checked">제주 체크</label>
+    <br>
+    <span> 체크한 지역 : {{checked}} </span> -->
+    <label><input type="radio" v-bind:value="radio[0]" v-model="picked">서울 선택</label>
+    <label><input type="radio" v-bind:value="radio[1]" v-model="picked">부산 선택</label>
+    <label><input type="radio" v-bind:value="radio[2]" v-model="picked">제주 선택</label>
+    <br>
+    <span> 선택한 지역 : {{picked}} </span>
 </template>
 
 <script>
@@ -11,15 +21,15 @@ export default {
     components: {},
     data() {
         return {
-            valueModel: 'South Korea',
-            valueModel2 : 12,
+            city: "064",
+            checked: [],
+            picked: "",
+            radio: ["서울!", "부산~", "제주:)"]
         }
     },
     setup() {},
     created() {},
-    mounted() {
-        console.log(this.valueModel2);
-    },
+    mounted() {},
     unmounted() {},
     methods: {
 
