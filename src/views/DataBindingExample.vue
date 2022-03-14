9edbff64d2413aa72a99ddf209932464337acf1c
manyyeon <udayeon@naver.com>
2022-03-15 03:23:31 +0900

    prettier, eslint

--- src/views/DataBindingExample.vue
+++ src/views/DataBindingExample.vue
@@ -1,39 +1,35 @@
 <template>
-    <div>
-        <input type="text" v-model="lastName" />
-        <input type="text" v-model="firstName" />
-        <h1>Hello, {{fullName}}</h1>
-        <h1>Hello, {{fullName}}</h1>
-        <h1>Hello, {{fullName}}</h1>
-        <h1>Hello, {{fullName}}</h1>
-        <h1>Hello, {{fullName}}</h1>
-    </div>
+  <div>
+    <input type="text" v-model="lastName" />
+    <input type="text" v-model="firstName" />
+    <h1>Hello, {{ fullName }}</h1>
+    <h1>Hello, {{ fullName }}</h1>
+    <h1>Hello, {{ fullName }}</h1>
+    <h1>Hello, {{ fullName }}</h1>
+    <h1>Hello, {{ fullName }}</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    components: {},
-    data() {
-        return {
-            firstName: '재석',
-            lastName: '유',
-        }
+  components: {},
+  data() {
+    return {
+      firstName: "재석",
+      lastName: "유",
+    };
+  },
+  computed: {
+    fullName() {
+      return this.lastName + this.firstName;
     },
-    computed: {
-        fullName() {
-            return this.lastName + this.firstName;
-        }
-    },
-    setup() {},
-    created() {},
-    mounted() {},
-    unmounted() {},
-    methods: {
-
-    }
-}
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
