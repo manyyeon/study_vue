d99232e85c11b2675c6115ba31e6807bded2edf4
manyyeon <udayeon@naver.com>
2022-03-14 19:18:10 +0900

    table 렌더링

--- src/views/DataBindingExample.vue
+++ src/views/DataBindingExample.vue
@@ -1,8 +1,23 @@
 <template>
     <div>
-        <select v-model="selectedCity">
-            <option :value="city.v" :key="i" v-for="(city,i) in options">{{city.t}}</option>
-        </select>
+        <table>
+            <thead>
+                <tr>
+                    <th>제품명</th>
+                    <th>제품가격</th>
+                    <th>배송비</th>
+                    <th>제품카테고리</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr :key="i" v-for="(product, i) in productList">
+                    <td>{{product.product_name}}</td>
+                    <td>{{product.price}}</td>
+                    <td>{{product.delivery_price}}</td>
+                    <td>{{product.category}}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -11,12 +26,10 @@ export default {
     components: {},
     data() {
         return {
-            options: [
-                {v:'02', t:'서울'},
-                {v:'21', t:'부산'},
-                {v:'064', t:'제주'},
-            ],
-            selectedCity: '064'
+            productList: [
+                {product_name: '기계식 키보드', price: 25000, delivery_price:5000, category:'전자제품'},
+                {product_name: '마우스', price: 9000, delivery_price:5000, category:'전자제품'}
+            ]
         }
     },
     setup() {},
