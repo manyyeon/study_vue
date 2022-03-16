8f01d1d880e2a911f913b028fa020b41c3b58b95
manyyeon <udayeon@naver.com>
2022-03-16 11:48:27 +0900

    조회화면, 저장화면 실습 - search

--- src/views/DataBindingExample.vue
+++ src/views/DataBindingExample.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
-    <input type="text" v-model.number="x1" />
-    <input type="text" v-model.number="x2" />
-    <input type="text" v-model="y" />
+    <select v-model="selectedCity">
+      <option :value="city.code" :key="i" v-for="(city, i) in cityList">
+        {{ city.name }}
+      </option>
+    </select>
+    <input type="text" v-model="userName" @keyup.enter="searchUserList" />
+    <button type="button" @click="searchUserList">조회</button>
+  </div>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>이름</th>
+          <th>나이</th>
+          <th>직업</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="i" v-for="(person, i) in userList">
+          <td>{{ person.name }}</td>
+          <td>{{ person.age }}</td>
+          <td>{{ person.job }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -11,24 +33,45 @@ export default {
   components: {},
   data() {
     return {
-      x1: 0,
-      x2: 0,
-      y: 0,
+      cityList: [
+        { name: "서울", code: "02" },
+        { name: "부산", code: "21" },
+        { name: "제주", code: "064" },
+      ],
+      selectedCity: "",
+      userName: "",
+      userList: [],
     };
-  },
-  watch: {
-    x1() {
-      this.y = this.x1 + this.x2;
-    },
-    x2() {
-      this.y = this.x1 + this.x2;
-    },
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {},
+  methods: {
+    searchUserList() {
+      // const selectedCity = this.selectedCity;
+      // const userName = this.userName;
+      const params = {
+        selectedCity: this.selectedCity,
+        userName: this.userName,
+      };
+      console.log(params);
+
+      this.userList = this.getUserList();
+    },
+    getUserList(params) {
+      console.log(params);
+
+      let list = [
+        { name: "유재석", age: 50, job: "연예인" },
+        { name: "하하", age: 30, job: "연예인" },
+        { name: "양세찬", age: 35, job: "연예인" },
+        { name: "김종국", age: 40, job: "연예인" },
+      ];
+
+      return list;
+    },
+  },
 };
 </script>
 
