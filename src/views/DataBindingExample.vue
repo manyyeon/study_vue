374b402e280c65f4f58d1918c647653d920b53e4
manyyeon <udayeon@naver.com>
2022-03-16 12:09:11 +0900

    조회화면, 저장화면 실습 - 저장

--- src/views/DataBindingExample.vue
+++ src/views/DataBindingExample.vue
@@ -1,30 +1,9 @@
 <template>
   <div>
-    <select v-model="selectedCity">
-      <option :value="city.code" :key="i" v-for="(city, i) in cityList">
-        {{ city.name }}
-      </option>
-    </select>
-    <input type="text" v-model="userName" @keyup.enter="searchUserList" />
-    <button type="button" @click="searchUserList">조회</button>
-  </div>
-  <div>
-    <table>
-      <thead>
-        <tr>
-          <th>이름</th>
-          <th>나이</th>
-          <th>직업</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr :key="i" v-for="(person, i) in userList">
-          <td>{{ person.name }}</td>
-          <td>{{ person.age }}</td>
-          <td>{{ person.job }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <input type="text" v-model="userInfo.name" />
+    <input type="text" v-model.number="userInfo.age" />
+    <input type="text" v-model="userInfo.job" />
+    <button type="button" @click="saveUserInfo">저장</button>
   </div>
 </template>
 
@@ -33,14 +12,11 @@ export default {
   components: {},
   data() {
     return {
-      cityList: [
-        { name: "서울", code: "02" },
-        { name: "부산", code: "21" },
-        { name: "제주", code: "064" },
-      ],
-      selectedCity: "",
-      userName: "",
-      userList: [],
+      userInfo: {
+        name: "",
+        age: 0,
+        job: "",
+      },
     };
   },
   setup() {},
@@ -48,28 +24,23 @@ export default {
   mounted() {},
   unmounted() {},
   methods: {
-    searchUserList() {
-      // const selectedCity = this.selectedCity;
-      // const userName = this.userName;
-      const params = {
-        selectedCity: this.selectedCity,
-        userName: this.userName,
-      };
-      console.log(params);
+    saveUserInfo() {
+      if (this.userInfo.name == "") {
+        return alert("사용자 이름을 입력하세요.");
+      }
+      if (this.userInfo.age == 0 || this.age == "") {
+        return alert("나이를 입력하세요.");
+      }
 
-      this.userList = this.getUserList();
+      const r = this.saveData(this.userInfo);
+      if (r == "S") {
+        alert("사용자 정보가 생성되었습니다.");
+      }
     },
-    getUserList(params) {
+    saveData(params) {
       console.log(params);
-
-      let list = [
-        { name: "유재석", age: 50, job: "연예인" },
-        { name: "하하", age: 30, job: "연예인" },
-        { name: "양세찬", age: 35, job: "연예인" },
-        { name: "김종국", age: 40, job: "연예인" },
-      ];
-
-      return list;
+      const r = "S";
+      return r;
     },
   },
 };
