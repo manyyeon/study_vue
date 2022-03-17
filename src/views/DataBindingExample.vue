a2dd96bf506e068f41f6e69a74ad88b0caa912d2
manyyeon <udayeon@naver.com>
2022-03-17 10:11:35 +0900

    컴포넌트 안에 다른 컴포넌트

--- src/views/DataBindingExample.vue
+++ src/views/DataBindingExample.vue
@@ -1,49 +1,24 @@
 <template>
   <div>
-    <input type="text" v-model="userInfo.name" />
-    <input type="text" v-model.number="userInfo.age" />
-    <input type="text" v-model="userInfo.job" />
-    <button type="button" @click="saveUserInfo">저장</button>
+    <PageTitle title="부모 컴포넌트에서 전송할 페이지 타이틀" />
   </div>
 </template>
 
 <script>
+import PageTitle from "../components/PageTitle.vue";
+
 export default {
-  components: {},
+  components: { PageTitle },
   data() {
     return {
-      userInfo: {
-        name: "",
-        age: 0,
-        job: "",
-      },
+      sampleData: "",
     };
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {
-    saveUserInfo() {
-      if (this.userInfo.name == "") {
-        // return alert("사용자 이름을 입력하세요.");
-        return this.$swal("사용자 이름을 입력하세요");
-      }
-      if (this.userInfo.age == 0 || this.age == "") {
-        return alert("나이를 입력하세요.");
-      }
-
-      const r = this.saveData(this.userInfo);
-      if (r == "S") {
-        alert("사용자 정보가 생성되었습니다.");
-      }
-    },
-    saveData(params) {
-      console.log(params);
-      const r = "S";
-      return r;
-    },
-  },
+  methods: {},
 };
 </script>
 
