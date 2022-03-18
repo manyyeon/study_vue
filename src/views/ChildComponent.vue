8d4449659917fc3403b20ac44323c3fac8294512
manyyeon <udayeon@naver.com>
2022-03-18 09:37:42 +0900

    부모 컴포넌트에서 자식 컴포넌트의 이벤트  발생시키기

--- src/views/ChildComponent.vue
+++ src/views/ChildComponent.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
-    <div>likes: {{ likes }}</div>
-    <div>isOk: {{ isOk }}</div>
-    <div>commentIds : {{ commentIds }}</div>
-    <div>author name : {{ author.name }}</div>
+    <button type="button" @click="childFunc" ref="child_btn">
+      자식에 있는 클릭
+    </button>
   </div>
 </template>
 
@@ -11,31 +10,17 @@
 export default {
   components: {},
   data() {
-    return {
-      sampleData: "",
-    };
-  },
-  props: {
-    likes: {
-      type: Number,
-      default: 0,
-    },
-    isOk: {
-      type: Boolean,
-      default: false,
-    },
-    commentIds: {
-      type: Array,
-    },
-    author: {
-      type: Object,
-    },
+    return {};
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {},
+  methods: {
+    childFunc() {
+      alert("부모 컴포넌트에서 직접 발생시킨 이벤트");
+    },
+  },
 };
 </script>
 
--- src/views/DataBindingExample.vue
+++ src/views/DataBindingExample.vue
@@ -1,36 +1,27 @@
 <template>
   <div>
-    <page-title :title="title" />
-    <ChildComponent
-      :likes="23"
-      :isOk="true"
-      :commentIds="commentIds"
-      :author="author"
-    />
+    <button type="button" @click="callChildFunc">부모에 있는 클릭</button>
+    <ChildComponent ref="child_component" />
   </div>
 </template>
 
 <script>
-import PageTitle from "../components/PageTitle.vue";
 import ChildComponent from "./ChildComponent.vue";
 
 export default {
-  components: { "page-title": PageTitle, ChildComponent },
+  components: { ChildComponent },
   data() {
-    return {
-      title: "부모 컴포넌트에서 전송할 타이틀",
-      commentIds: [1, 5, 2, 3],
-      author: {
-        name: "홍길동",
-        company: "회사",
-      },
-    };
+    return {};
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {},
+  methods: {
+    callChildFunc() {
+      this.$refs.child_component.$refs.child_btn.click();
+    },
+  },
 };
 </script>
 
