1e12510ba6e5b5d8ba5ac4a1cdba287794a6ad12
manyyeon <udayeon@naver.com>
2022-03-20 03:26:32 +0900

    vuex 예제 - 장바구니

--- src/store.js
+++ src/store.js
@@ -4,11 +4,23 @@ const store = createStore({
   state() {
     return {
       count: 0,
+      cart: [
+        { product_id: 1, product_name: "아이폰 거치대", category: "A" },
+        { product_id: 2, product_name: "블루투스 마우스", category: "B" },
+      ],
     };
   },
   mutations: {
     increment(state) {
       state.count = state.count + 1;
+    },
+  },
+  getters: {
+    cartCount: (state) => {
+      return state.cart.length;
+    },
+    productACount: (state) => {
+      return state.cart.filter((p) => p.category == "A").length;
     },
   },
 });
--- src/views/StoreAccess.vue
+++ src/views/StoreAccess.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <h1>장바구니: {{ cartCount }}개</h1>
+    <h1>장바구니 - category가 A인 것만 : {{ productACount }}개</h1>
     <h2>Count: {{ count }}</h2>
     <button type="button" @click="increment">+1</button>
   </div>
@@ -10,6 +12,12 @@ export default {
   computed: {
     count() {
       return this.$store.state.count;
+    },
+    cartCount() {
+      return this.$store.getters.cartCount;
+    },
+    productACount() {
+      return this.$store.getters.productACount;
     },
   },
   methods: {
